6b327089c365e0fe9b20189d1105a64542652fe0
Amitesh Ranjan <amiteshranjan5524@gmail.com>
2026-01-20 13:36:34 +0530

    add error middleware

---
+++ server/src/middlewares/error.middleware.js
@@ -0,0 +1,6 @@
+import { logger } from "../utils/logger.js";
+
+export default function errorHandler(err, req, res, next) {
+  logger.error("Unhandled error", { message: err.message, stack: err.stack });
+  res.status(500).json({ error: "Internal Server Error" });
+}
